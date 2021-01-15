6648777451fe33b3ee79b10f58a698d06d0d58b6
sholeh007 <sholeh09@outlook.com>
2021-01-15 14:55:09 +0900

    menambah serving static file

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
 import express from "express";
 import path from "path";
+import dir from "./helper/path.js";
 import adminRoute from "../src/routes/admin.js";
 import shopRoute from "../src/routes/shop.js";
 
 const app = express();
-const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use("/admin", adminRoute);
 app.use(shopRoute);
 
 // middleware for error 404
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "src", "views", "404.html"));
+  res.status(404).sendFile(path.join(dir, "../", "views", "404.html"));
 });
 
 app.listen(3000);
