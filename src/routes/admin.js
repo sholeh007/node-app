7de8fa486c374cdec9853a4b0f4c240bd95ac0d1
sholeh007 <sholeh09@outlook.com>
2021-01-16 09:04:37 +0900

    menambah package pug

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,15 +3,15 @@ import path from "path";
 import dir from "../helper/path.js";
 
 const Router = express.Router();
-const data = [];
+const product = [];
 
 Router.route("/add-product")
   .get((req, res) => {
     res.sendFile(path.join(dir, "../", "views", "add-product.html"));
   })
   .post((req, res) => {
-    data.push({ title: req.body.title });
+    product.push({ title: req.body.title });
     res.redirect("/");
   });
 
-export { Router, data };
+export { Router, product };
--- src/routes/shop.js
+++ src/routes/shop.js
@@ -1,10 +1,12 @@
 import express from "express";
 import path from "path";
 import dir from "../helper/path.js";
+import { product } from "./admin.js";
 
 const Route = express.Router();
 
-Route.get("/", (req, res, next) => {
+Route.get("/", (req, res) => {
+  console.log(product);
   res.sendFile(path.join(dir, "../", "views", "shop.html"));
 });
 
