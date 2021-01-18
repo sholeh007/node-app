12a681e4e5d45c5dec874f2a8e4a865949f67be5
sholeh007 <sholeh09@outlook.com>
2021-01-18 13:19:12 +0900

    membuat dinamic active class

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -7,7 +7,7 @@ const Route = express.Router();
 
 Route.get("/", (req, res) => {
   const data = product;
-  res.render("shop", { product: data, title: "Shop" });
+  res.render("shop", { product: data, title: "Shop", path: "/" });
 });
 
 export default Route;
